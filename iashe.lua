2e44ea9a2c00ce17f70b576c33ee7b9eb9d3df8a
ovior <loloviorhihi@gmail.com>
2016-06-16 22:35:08 -0400

    Create iashe.lua

---
+++ iashe.lua
@@ -0,0 +1,281 @@
+--[[ IASHE
+
+=> Skin Changer
+=>
+
+
+
+
+]]
+
+class "SxScriptUpdate"
+function SxScriptUpdate:__init(LocalVersion,UseHttps, Host, VersionPath, ScriptPath, SavePath, CallbackUpdate, CallbackNoUpdate, CallbackNewVersion,CallbackError)
+    self.LocalVersion = LocalVersion
+    self.Host = Host
+    self.VersionPath = '/BoL/TCPUpdater/GetScript'..(UseHttps and '5' or '6')..'.php?script='..self:Base64Encode(self.Host..VersionPath)..'&rand='..math.random(99999999)
+    self.ScriptPath = '/BoL/TCPUpdater/GetScript'..(UseHttps and '5' or '6')..'.php?script='..self:Base64Encode(self.Host..ScriptPath)..'&rand='..math.random(99999999)
+    self.SavePath = SavePath
+    self.CallbackUpdate = CallbackUpdate
+    self.CallbackNoUpdate = CallbackNoUpdate
+    self.CallbackNewVersion = CallbackNewVersion
+    self.CallbackError = CallbackError
+    AddDrawCallback(function() self:OnDraw() end)
+    self:CreateSocket(self.VersionPath)
+    self.DownloadStatus = 'Connect to Server for VersionInfo'
+    AddTickCallback(function() self:GetOnlineVersion() end)
+end
+
+function SxScriptUpdate:print(str)
+    print('<font color="#FFFFFF">'..os.clock()..': '..str)
+end
+
+function SxScriptUpdate:OnDraw()
+    if self.DownloadStatus ~= 'Downloading Script (100%)' and self.DownloadStatus ~= 'Downloading VersionInfo (100%)'then
+        DrawText('Download Status: '..(self.DownloadStatus or 'Unknown'),50,10,50,ARGB(0xFF,0xFF,0xFF,0xFF))
+    end
+end
+
+function SxScriptUpdate:CreateSocket(url)
+    if not self.LuaSocket then
+        self.LuaSocket = require("socket")
+    else
+        self.Socket:close()
+        self.Socket = nil
+        self.Size = nil
+        self.RecvStarted = false
+    end
+    self.Socket = self.LuaSocket.tcp()
+    self.Socket:settimeout(0, 'b')
+    self.Socket:settimeout(99999999, 't')
+    self.Socket:connect('sx-bol.eu', 80)
+    self.Url = url
+    self.Started = false
+    self.LastPrint = ""
+    self.File = ""
+end
+
+function SxScriptUpdate:Base64Encode(data)
+    local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    return ((data:gsub('.', function(x)
+        local r,b='',x:byte()
+        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if (#x < 6) then return '' end
+        local c=0
+        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+        return b:sub(c+1,c+1)
+    end)..({ '', '==', '=' })[#data%3+1])
+end
+
+function SxScriptUpdate:GetOnlineVersion()
+    if self.GotScriptVersion then return end
+
+    self.Receive, self.Status, self.Snipped = self.Socket:receive(1024)
+    if self.Status == 'timeout' and not self.Started then
+        self.Started = true
+        self.Socket:send("GET "..self.Url.." HTTP/1.0\r\nHost: sx-bol.eu\r\nUser-Agent: hDownload\r\n\r\n")
+    end
+
+    self.File = self.File .. (self.Receive or self.Snipped)
+    if self.File:find('</s'..'ize>') then
+        if not self.Size then
+            self.Size = tonumber(self.File:sub(self.File:find('<si'..'ze>')+6,self.File:find('</si'..'ze>')-1))
+        end
+        if self.File:find('<scr'..'ipt>') then
+            local _,ScriptFind = self.File:find('<scr'..'ipt>')
+            local ScriptEnd = self.File:find('</scr'..'ipt>')
+            if ScriptEnd then ScriptEnd = ScriptEnd - 1 end
+            local DownloadedSize = self.File:sub(ScriptFind+1,ScriptEnd or -1):len()
+            self.DownloadStatus = 'Downloading VersionInfo ('..math.round(100/self.Size*DownloadedSize,2)..'%)'
+        end
+    end
+    if self.File:find('</scr'..'ipt>') or self.Status == 'closed' then
+        local HeaderEnd, ContentStart = self.File:find('<scr'..'ipt>')
+        local ContentEnd, _ = self.File:find('</sc'..'ript>')
+        if not ContentStart or not ContentEnd then
+            if self.CallbackError and type(self.CallbackError) == 'function' then
+                self.CallbackError()
+            end
+        else
+            self.OnlineVersion = (Base64Decode(self.File:sub(ContentStart + 1,ContentEnd-1)))
+            self.OnlineVersion = tonumber(self.OnlineVersion)
+            if not self.OnlineVersion then
+                if self.CallbackError and type(self.CallbackError) == 'function' then
+                    self.CallbackError()
+                end
+            else
+                if self.OnlineVersion > self.LocalVersion then
+                    if self.CallbackNewVersion and type(self.CallbackNewVersion) == 'function' then
+                        self.CallbackNewVersion(self.OnlineVersion,self.LocalVersion)
+                    end
+                    self:CreateSocket(self.ScriptPath)
+                    self.DownloadStatus = 'Connect to Server for ScriptDownload'
+                    AddTickCallback(function() self:DownloadUpdate() end)
+                else
+                    if self.CallbackNoUpdate and type(self.CallbackNoUpdate) == 'function' then
+                        self.CallbackNoUpdate(self.LocalVersion)
+                    end
+                end
+            end
+        end
+        self.GotScriptVersion = true
+    end
+end
+
+function SxScriptUpdate:DownloadUpdate()
+    if self.GotSxScriptUpdate then return end
+    self.Receive, self.Status, self.Snipped = self.Socket:receive(1024)
+    if self.Status == 'timeout' and not self.Started then
+        self.Started = true
+        self.Socket:send("GET "..self.Url.." HTTP/1.0\r\nHost: sx-bol.eu\r\n\r\n")
+    end
+    if (self.Receive or (#self.Snipped > 0)) and not self.RecvStarted then
+        self.RecvStarted = true
+        self.DownloadStatus = 'Downloading Script (0%)'
+    end
+
+    self.File = self.File .. (self.Receive or self.Snipped)
+    if self.File:find('</si'..'ze>') then
+        if not self.Size then
+            self.Size = tonumber(self.File:sub(self.File:find('<si'..'ze>')+6,self.File:find('</si'..'ze>')-1))
+        end
+        if self.File:find('<scr'..'ipt>') then
+            local _,ScriptFind = self.File:find('<scr'..'ipt>')
+            local ScriptEnd = self.File:find('</scr'..'ipt>')
+            if ScriptEnd then ScriptEnd = ScriptEnd - 1 end
+            local DownloadedSize = self.File:sub(ScriptFind+1,ScriptEnd or -1):len()
+            self.DownloadStatus = 'Downloading Script ('..math.round(100/self.Size*DownloadedSize,2)..'%)'
+        end
+    end
+    if self.File:find('</scr'..'ipt>') or self.Status == 'closed' then
+        local HeaderEnd, ContentStart = self.File:find('<sc'..'ript>')
+        local ContentEnd, _ = self.File:find('</scr'..'ipt>')
+        if not ContentStart or not ContentEnd then
+            if self.CallbackError and type(self.CallbackError) == 'function' then
+                self.CallbackError()
+            end
+        else
+            local newf = self.File:sub(ContentStart+1,ContentEnd-1)
+            local newf = newf:gsub('\r','')
+            if newf:len() ~= self.Size then
+                if self.CallbackError and type(self.CallbackError) == 'function' then
+                    self.CallbackError()
+                end
+                self.GotSxScriptUpdate = true
+                return
+            end
+            local newf = Base64Decode(newf)
+            if type(load(newf)) ~= 'function' then
+                if self.CallbackError and type(self.CallbackError) == 'function' then
+                    self.CallbackError()
+                end
+            else
+                local f = io.open(self.SavePath,"w+b")
+                f:write(newf)
+                f:close()
+                if self.CallbackUpdate and type(self.CallbackUpdate) == 'function' then
+                    self.CallbackUpdate(self.OnlineVersion,self.LocalVersion)
+                end
+            end
+        end
+        self.GotSxScriptUpdate = true
+    end
+end
+
+---------------------------------------------------------------------------------------
+class 'iAshe'
+function iAshe:__init()
+    self.version = 1.0
+    print('<font color=\'#FF794C\'><b>iAshe:</b></font> <font color=\'#FFDFBF\'>Version '..self.version..' loaded</font>')
+    local ToUpdate = {}
+    ToUpdate.Version = self.version
+    ToUpdate.UseHttps = true
+    ToUpdate.Host = "raw.githubusercontent.com"
+    ToUpdate.VersionPath = "/ovior/BoL/master/iAshe.Version"
+    ToUpdate.ScriptPath = "/ovior/BoL/master/iAshe.lua"
+    ToUpdate.SavePath = SCRIPT_PATH.."/iAshe.lua"
+    ToUpdate.CallbackUpdate = function(NewVersion,OldVersion) print("<font color=\"#FF794C\"><b>iAshe: </b></font> <font color=\"#FFDFBF\">Updated to "..NewVersion..". Please Reload with 2x F9</b></font>") end
+    ToUpdate.CallbackNoUpdate = function(OldVersion) print("<font color=\"#FF794C\"><b>iAshe: </b></font> <font color=\"#FFDFBF\">No Updates Found</b></font>") end
+    ToUpdate.CallbackNewVersion = function(NewVersion) print("<font color=\"#FF794C\"><b>iAshe: </b></font> <font color=\"#FFDFBF\">New Version found ("..NewVersion.."). Please wait until its downloaded</b></font>") end
+    ToUpdate.CallbackError = function(NewVersion) print("<font color=\"#FF794C\"><b>iAshe: </b></font> <font color=\"#FFDFBF\">Error while Downloading. Please try again.</b></font>") end
+    SxScriptUpdate(ToUpdate.Version,ToUpdate.UseHttps, ToUpdate.Host, ToUpdate.VersionPath, ToUpdate.ScriptPath, ToUpdate.SavePath, ToUpdate.CallbackUpdate,ToUpdate.CallbackNoUpdate, ToUpdate.CallbackNewVersion,ToUpdate.CallbackError)
+end
+
+----------------------------------------------------------------------------------------
+
+
+ABL = 0
+sBuy = false
+tUpgrade = false
+
+
+
+if myHero.charName ~= "Ashe" then return end
+
+-- => Skin Changer
+SkinC = {"Classic Ashe", "(L)Freljord Ashe", "Sherwood Forest Ashe", "Woad Ashe", "(L)Queen Ashe", "Amethyst Ashe", "Heartseeker Ashe", "Marauder Ashe"}
+
+function _GetSkin()
+        if core.SkinChanger == 1 then SetSkin(myHero, 0) end
+        if core.SkinChanger == 2 then SetSkin(myHero, 1) end
+        if core.SkinChanger == 3 then SetSkin(myHero, 2) end
+        if core.SkinChanger == 4 then SetSkin(myHero, 3) end
+        if core.SkinChanger == 5 then SetSkin(myHero, 4) end
+        if core.SkinChanger == 6 then SetSkin(myHero, 5) end
+        if core.SkinChanger == 7 then SetSkin(myHero, 6) end
+        if core.SkinChanger == 8 then SetSkin(myHero, 7) end
+end
+
+--
+
+-- => Auto Leveler
+
+asheABL = {_W, _E, _W, _Q, _W, _R, _W, _Q, _W, _Q, _R, _Q, _Q, _E, _E, _R, _E, _E}
+
+function _getLevel()
+        if myHero.level > ABL then
+                ABL = ABL + 1
+                LevelSpell(asheABL[ABL])
+        end
+end
+
+--
+
+-- => Start Items
+
+function _StartItems()
+
+        if sBuy == false and myHero.level == 1 and GetTickCount() - TickStart > 2000 then
+                BuyItem(1055)
+                BuyItem(2003)
+                BuyItem(3340)
+                sBuy = true
+        end
+
+        if myHero.level > 9 and tUpgrade == false then
+                BuyItem(3363)
+                tUpgrade = true
+        end
+end
+
+ --
+
+-- => MAIN FUCTIONS
+
+function OnLoad()
+        _CoreMenu()
+        TickStart = GetTickCount()
+end
+
+function OnTick()
+        _StartItems()
+        _GetSkin()
+        _getLevel()
+end
+
+
+function _CoreMenu()
+        core = scriptConfig("iAshe", "iAshe")
+        core:addParam("SkinChanger", "Ashe Changer: ", SCRIPT_PARAM_LIST, 1, SkinC)
+end
